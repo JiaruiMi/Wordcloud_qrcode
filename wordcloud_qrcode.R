1b2c9923c15b3262bfe018a4e038698873f43b02
JiaruiMi <mjrpumc@outlook.com>
2018-03-16 17:50:00 +0100

    plot

--- wordcloud_qrcode.R
+++ wordcloud_qrcode.R
@@ -24,12 +24,33 @@ data$V2 <- seq(from = n, to = 1, by = -1)
 head(data);dim(data)
 data <- rename(data, word = V1, frequency = V2)
 data[data$word == 'lncRNA',2] = 150
-data <- rbind(data, data, data) # we can do this step several times to make the number of word a little bit larger
+data <- rbind(data, data, data, data, data, data, data, data) # we can do this step several times to make the number of word a little bit larger
 
 ## Make the plot
 wordcloud2(data,size=0.5,fontFamily="Segoe UI",fontWeight = 'bold', color = 'random-dark',
            backgroundColor = "white", minRotation = -pi/4, maxRotation =pi/4, shuffle = F,
            figPath = '/Users/mijiarui/R_bioinformatics_project/Master_thesis_project/Wordcloud/doctorcap.png')
 
+wordcloud2(data,size=0.5,fontFamily="Segoe UI",fontWeight = 'bold', color = 'random-dark',
+           backgroundColor = "white", minRotation = -pi/4, maxRotation =pi/4, shuffle = F,
+           figPath = '/Users/mijiarui/R_bioinformatics_project/Master_thesis_project/Wordcloud/doctorcap1.png')
+
+wordcloud2(data,size=0.5,fontFamily="Segoe UI",fontWeight = 'bold', color = 'random-dark',
+           backgroundColor = "white", minRotation = -pi/4, maxRotation =pi/4, shuffle = F,
+           figPath = '/Users/mijiarui/R_bioinformatics_project/Master_thesis_project/Wordcloud/dogpaw.jpg')
+
+wordcloud2(data,size=0.5,fontFamily="Segoe UI",fontWeight = 'bold', color = 'random-dark',
+           backgroundColor = "white", minRotation = -pi/4, maxRotation =pi/4, shuffle = F,
+           figPath = '/Users/mijiarui/R_bioinformatics_project/Master_thesis_project/Wordcloud/dog.jpg')
+
 ### The point is the figure you choose is better to have large areas so that the words can reside in, otherwise
-### the plot would be very ungly.
+### the plot would be very ungly. Another thing is the object should be dark in the picture, the very classical
+### negative example is 'aula medica'.
+
+
+#============================================================================
+#
+#                             qrcode generation
+#
+#============================================================================
+library('yyplot')
