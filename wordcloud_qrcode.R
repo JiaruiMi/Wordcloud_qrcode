5f4c2ef38f208447ee0eb9c22a9d70a5ae4137f7
JiaruiMi <mjrpumc@outlook.com>
2018-03-16 13:51:14 +0100

    First commit

---
+++ wordcloud_qrcode.R
@@ -0,0 +1,11 @@
+#============================================================================
+#
+#                             Wordcloud Drawing
+#
+#============================================================================
+
+### Official blog:
+### https://github.com/Lchiffon/wordcloud2
+### http://lchiffon.github.io/2016/06/01/wordcloud2.html
+
+## install the package from github
